7c8e943e01a704b51a57071a7040ce584622974c
winsentrobot008 <winsentrobot008@users.noreply.github.com>
2026-05-16 21:46:52 +0200

    Add backend API route

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   async function testAPI() {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/test");
+    const res = await fetch("/api/test");
     const data = await res.json();
     console.log(data);
     alert(JSON.stringify(data));
